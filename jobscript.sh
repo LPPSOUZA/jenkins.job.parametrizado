02daa98b7ef28071732dc0e0cc303de83add20d5
LPPSOUZA <161338708+LPPSOUZA@users.noreply.github.com>
2024-04-14 21:09:49 -0300

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -1,7 +1,3 @@
 echo "Bem-vindo(a) ao planeta $planeta"
-if [ "$agente" = true ]
-then
-	echo "Prepare-se para a missão, agente secreto. Que a força te acompanhe!"
-else
-	echo "Desfrute do planeta $planeta, humano $nome e tente sobreviver"
-fi
+echo "Prepare-se para a missão, agente secreto. Que a força te acompanhe!"
+echo "Desfrute do planeta $planeta, humano $nome e tente sobreviver"
